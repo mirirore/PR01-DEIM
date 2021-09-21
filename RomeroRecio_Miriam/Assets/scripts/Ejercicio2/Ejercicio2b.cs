01611cddbb5e95b4cc48574eda10c0afc935a035
mirirore <77435665+mirirore@users.noreply.github.com>
2021-09-21 12:12:42 +0200

    ejercicio2b

--- RomeroRecio_Miriam/Assets/scripts/Ejercicio2/Ejercicio2b.cs
+++ RomeroRecio_Miriam/Assets/scripts/Ejercicio2/Ejercicio2b.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Ejercicio2b : MonoBehaviour
 {
+    int n;
+    int x;
     // Start is called before the first frame update
     void Start()
     {
-        
+        /* while
+        n = 0;
+
+        while (n < 10)
+        {
+          print(n);
+          n++;
+        }
+        */
+
+
+        //for con concatenación
+        for (int x = 0; x < 10; x++)
+        {
+            print("x vale:" + x);
+        }
+
     }
 
     // Update is called once per frame
