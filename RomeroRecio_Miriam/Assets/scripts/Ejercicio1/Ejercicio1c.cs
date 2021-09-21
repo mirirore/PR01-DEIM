196b991f480777e6d00dd90705c2b80d7f355b98
mirirore <77435665+mirirore@users.noreply.github.com>
2021-09-21 10:13:18 +0200

    ejercicio1c

--- RomeroRecio_Miriam/Assets/scripts/Ejercicio1/Ejercicio1c.cs
+++ RomeroRecio_Miriam/Assets/scripts/Ejercicio1/Ejercicio1c.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Ejercicio1c : MonoBehaviour
 {
+    [SerializeField] float num1 = 5f;
+    [SerializeField] float num2 = 5f;
+    private float resultado;
+
     // Start is called before the first frame update
     void Start()
     {
+        resultado = num1 * num2;
+        print(resultado);
+
         
     }
 
