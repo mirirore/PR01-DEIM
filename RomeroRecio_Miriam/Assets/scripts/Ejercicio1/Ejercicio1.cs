8bd1366c396d80371953aa8869d894893f93af1a
mirirore <77435665+mirirore@users.noreply.github.com>
2021-09-21 09:53:40 +0200

    ejercicio 1a

---
+++ RomeroRecio_Miriam/Assets/scripts/Ejercicio1/Ejercicio1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    private string yo="miri";
+    private int x = 3;
+    private float y = 2f;
+    private bool z = true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(yo);
+        print(x);
+        print(y);
+        print(z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
